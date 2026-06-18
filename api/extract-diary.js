4398d3fa118c5989c79da03d25908d8a1bf50042
45w4s2g96w-ship-it <45w4s2g96w@privaterelay.appleid.com>
2026-06-19 08:44:46 +0900

    Add files via upload

---
+++ api/extract-diary.js
@@ -0,0 +1,28 @@
+import { handleExtractDiary } from '../lib/core.js';
+import { toWebRequest, sendWebResponse } from '../lib/adapter.js';
+
+export const config = {
+  regions: ['icn1'],
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handler(req, res) {
+  const url = new URL(req.url, `https://${req.headers.host}`);
+
+  if (url.searchParams.get('secret') !== process.env.AUTH_SECRET) {
+    res.status(401).send('Unauthorized');
+    return;
+  }
+
+  const notionHeaders = {
+    'Authorization': `Bearer ${process.env.NOTION_TOKEN}`,
+    'Notion-Version': '2022-06-28',
+    'Content-Type': 'application/json'
+  };
+
+  const webRequest = toWebRequest(req);
+  const response = await handleExtractDiary(webRequest, process.env, notionHeaders);
+  await sendWebResponse(response, res);
+}
